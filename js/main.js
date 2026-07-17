7a5e25a1a4e617aeee54fa2e18d84b409deaf07b
Czarek6688 <czarekrosloniec2006@gmail.com>
2026-07-17 13:09:35 +0200

    dark mode

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 let toDoApp = document.getElementById("to-do-app");
 let text1 = document.getElementById("text");
 let shoppingList = document.getElementById("shopping-list");
+let przycisk1 = document.getElementById("add");
+let przycisk2 = document.getElementById("delete");
+let przycisk3 = document.getElementById("switch");
+let body = document.querySelector("body");
 
-toDoApp.addEventListener("submit", function (event) {
+przycisk1.addEventListener("click", function (event) {
   event.preventDefault();
   console.log(text1.value);
 
@@ -14,4 +18,16 @@ toDoApp.addEventListener("submit", function (event) {
     alert("Wpisz produkt");
     return;
   }
+});
+
+przycisk2.addEventListener("click", function () {
+  const ostatni = shoppingList.lastElementChild;
+
+  if (ostatni) {
+    ostatni.remove();
+  }
+});
+
+przycisk3.addEventListener("click", function () {
+  body.classList.toggle("black");
 });
