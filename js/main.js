a40348a3ad97a7a2dbdf7b293922c7a56fe5a7b4
Czarek6688 <czarekrosloniec2006@gmail.com>
2026-07-20 10:50:36 +0200

    przycisk usuń i zrobione

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let toDoApp = document.getElementById("to-do-app");
 let text1 = document.getElementById("text");
 let shoppingList = document.getElementById("shopping-list");
 let przycisk1 = document.getElementById("add");
-let przycisk2 = document.getElementById("delete");
 let przycisk3 = document.getElementById("switch");
 let body = document.querySelector("body");
 
@@ -14,17 +13,25 @@ przycisk1.addEventListener("click", function (event) {
   newItem.textContent = text1.value;
   shoppingList.appendChild(newItem);
 
+  const newCheckBoxElement = document.createElement("input");
+  newItem.appendChild(newCheckBoxElement);
+  newCheckBoxElement.setAttribute("type", "checkbox");
+
+  newCheckBoxElement.addEventListener("change", function () {
+    newItem.classList.toggle("done");
+  });
+
+  const deleteItem = document.createElement("button");
+  deleteItem.textContent = "Usuń";
+  newItem.appendChild(deleteItem);
+
+  deleteItem.addEventListener("click", function () {
+    deleteItem.parentElement.remove();
+  });
+
   if (text1.value.trim() === "") {
     alert("Wpisz produkt");
     return;
-  }
-});
-
-przycisk2.addEventListener("click", function () {
-  const ostatni = shoppingList.lastElementChild;
-
-  if (ostatni) {
-    ostatni.remove();
   }
 });
 
